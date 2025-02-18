950463ddc4b7b6328f575195b1b6f386ae53bc3a
Andrew Kurylyk <andronniy7@gmail.com>
2025-02-18 22:53:35 +0200

    One

--- hw6/main.js
+++ hw6/main.js
@@ -11,4 +11,40 @@ const users = [
     {name: "Charlotte", age: 32, status: true}
 ];
 
-users.forEach(value => console.log (value));
+// users.forEach(value => console.log (value));
+// let filteredUsers =  users.filter(value => value.status === true);
+// console.log(filteredUsers);
+// let printedUsers = users.map((value, index) => {
+//     return { id: index + 1, name: value.name, age: value.age, status: value.status };
+// });
+// console.log(printedUsers);
+// let find = users.find(value => value.name === "Sophia");
+// console.log(find);
+// console.log(users.every(value => value.status));
+// console.log(users.some(value => value.status));
+// let sortOne = users.sort((user1, user2) => {
+//     return user1.age - user2.age;
+// });
+// console.log(sortOne);
+// let sortTwo = users.sort((user1, user2) => {
+//     return user2.age - user1.age;
+// });
+// console.log(sortTwo);
+
+let sortByName = users.sort((a, b) => {
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name === b.name) {
+        return 0;
+    }
+});
+
+console.log(sortByName);
+
+
+
+
