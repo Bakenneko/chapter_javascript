6286dbc83d3ac8ba2820cf531f81f044b0b62849
Andrew Kurylyk <andronniy7@gmail.com>
2025-02-18 21:19:05 +0200

    One

---
+++ hw6/main.js
@@ -0,0 +1,14 @@
+const users = [
+    {name: "John", age: 25, status: false},
+    {name: "Alice", age: 30, status: true},
+    {name: "Bob", age: 22, status: false},
+    {name: "Emma", age: 28, status: false},
+    {name: "Mike", age: 35, status: true},
+    {name: "Sophia", age: 27, status: true},
+    {name: "Daniel", age: 40, status: false},
+    {name: "Olivia", age: 29, status: true},
+    {name: "Liam", age: 24, status: true},
+    {name: "Charlotte", age: 32, status: true}
+];
+
+users.forEach(value => console.log (value));
