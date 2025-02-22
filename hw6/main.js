7426130aac2ff8946666ae209749b757fa36e9ee
Andrew Kurylyk <andronniy7@gmail.com>
2025-02-22 09:50:48 +0200

    One

--- hw6/main.js
+++ hw6/main.js
@@ -106,3 +106,12 @@
 //
 // let user2 = new User1('Jack',27,true,'Liza',24,true);
 // console.log(user2);
+let now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
+console.log(now.getTime());
